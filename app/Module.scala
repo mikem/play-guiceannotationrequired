4c226e3f01659bd65df0a96fc9fc7ff92c1aa57b
Mike Mazur <mmazur@gmail.com>
2016-08-04 11:20:02 +0800

    Configure Guice to require @Inject() annotation

--- app/Module.scala
+++ app/Module.scala
@@ -16,6 +16,8 @@ import services.{ApplicationTimer, AtomicCounter, Counter}
 class Module extends AbstractModule {
 
   override def configure() = {
+    binder().requireAtInjectOnConstructors()
+
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
     // Ask Guice to create an instance of ApplicationTimer when the
